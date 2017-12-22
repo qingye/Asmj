db0e4c2ccc527270364e02cc41d8e85c433b5a73
qingye <qingye_love@qq.com>
2017-12-22 19:11:25 +0800

    demo

--- app/src/main/java/com/asm/demo/MainActivity.java
+++ app/src/main/java/com/asm/demo/MainActivity.java
@@ -2,6 +2,10 @@ package com.asm.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e("DEMO", "textView onClick");
+            }
+        });
+        textView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                Log.e("DEMO", "textView onTouch");
+                return false;
+            }
+        });
     }
 }
